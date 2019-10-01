ae51d81de12bde0cee3fd581015a2dff91b7c2ee
Lenocka <lvutina.en@gmail.com>
2019-10-01 10:11:47 +0300

    fix: 1 fall test left

--- src/index.js
+++ src/index.js
@@ -4,21 +4,27 @@ module.exports = function check(str, bracketsConfig) {
   let openBrackets=[];
   for (let i=0; i<str.length; i++){
     for (let j=0;j<bracketsConfig.length;j++){
-     // console.log("i:" + i + " j:" + j + str[i]+bracketsConfig[j][0]);
+      //console.log("i:" + i + " j:" + j + str[i]+bracketsConfig[j][0]);
       if (str[i]==bracketsConfig[j][0]){
-        openBrackets.push(str[i]);
-       //  console.log("openBrackets: " + openBrackets);
+        if (str[i]==bracketsConfig[j][1]&str[i]==openBrackets[openBrackets.length-1]){
+          openBrackets.pop();
+        }  
+        else openBrackets.push(str[i])  
+        //console.log("openBrackets: " + openBrackets);
+         break;
       }
       if (str[i]==bracketsConfig[j][1]){
-       // console.log("i:" + i + " j:" + j + str[i] + " " + bracketsConfig[j][1]);
+       //console.log("i:" + i + " j:" + j + str[i] + " " + bracketsConfig[j][1]);
         let lastBracket=openBrackets.pop();
+        //console.log("openBrackets: " + openBrackets)
         if (bracketsConfig[j][0]!=lastBracket){ 
           return false;
         }
+        break;
       }
     }
   }
   return true;
 }
 
- //console.log(check('|(|)',[['(', ')'], ['|', '|']] ));
+//console.log(check('|()|(||)||', [['(', ')'], ['|', '|']] ));
